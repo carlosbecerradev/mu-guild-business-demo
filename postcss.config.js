e1c56d176f98f2cfc2f24dffe7664c14d2ce1e7f
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-11-24 17:46:23 -0500

    :rocket: Implementando tailwindcss

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+// postcss.config.js
+module.exports = {
+    plugins: {
+      tailwindcss,
+      autoprefixer,
+    }
+  }
