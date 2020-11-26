313e3550a25b4de7bbf3e78bbab59d0ed8da4e04
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-11-26 02:54:28 -0500

    :rocket: Ruta para la vista 'login'.

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,11 @@ const routes = [
     name: 'Home',
     component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue')
   },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
+  },
 ]
 
 const router = new VueRouter({
