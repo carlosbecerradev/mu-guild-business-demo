b0107db17d2c6f9a5057fb0d2fc1c96b54c64885
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-11-27 18:43:30 -0500

    :rocket: Usando el 'time_ago'.

--- src/views/MUServers.vue
+++ src/views/MUServers.vue
@@ -97,7 +97,7 @@
                     d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
                   />
                 </svg>
-                {{ mu_server.created }}
+                {{ useTimeAgo(mu_server.created) }}
               </div>
             </div>
           </div>
@@ -132,6 +132,7 @@
 
 <script>
 import { mapGetters, mapActions } from "vuex";
+import useTimeAgo from '@/helpers/time_ago'
 
 export default {
   computed: {
@@ -139,6 +140,7 @@ export default {
   },
   methods: {
     ...mapActions(["getAllMUServers"]),
+    useTimeAgo,
   },
   created() {
     this.getAllMUServers();
