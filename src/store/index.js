fbe01428fad823353bae8257470b425495a4cfdb
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-11-26 19:34:48 -0500

    :rocket: Se hace uso de la API 'user_accounts' y métodos para manejar el 'login'.

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import user_accounts from './user_accounts'
 
 Vue.use(Vuex)
 
@@ -11,5 +12,6 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
+    user_accounts,
   }
 })
---
+++ src/store/user_accounts.js
@@ -0,0 +1,59 @@
+import user_accounts_api from '@/api/user_accounts'
+import router from '@/router'
+
+const state = {
+  all: [],
+  username: null,
+}
+
+const actions = {
+  getAllUserAccounts({ commit }) {
+    user_accounts_api.getAll(users_account => {
+      commit('recieve_user_accounts', users_account)
+    })
+  },
+  login({ commit }, user_account) {
+    const response = user_accounts_api.validateAccount(user_account)
+    if (response != null) {
+      localStorage.setItem('username', user_account.username)
+      commit('set_username', user_account.username)
+      router.push('/')
+      console.log(response)
+    }
+  },
+  logout({commit}) {
+    localStorage.removeItem('username')
+    commit('set_username', null)
+    router.push('/login')
+  },
+  isAuthenticated({commit}) {
+    if(localStorage.getItem('username')){
+      commit('set_username', localStorage.getItem('username'))
+    }
+  }
+}
+
+const mutations = {
+  recieve_user_accounts(state, user_accounts) {
+    state.all = user_accounts
+  },
+  set_username(state, payload){
+    state.username = payload
+  },
+}
+
+const getters = {
+  allUserAccounts(state) {
+    return state.all
+  },
+  getUsername(state){
+    return state.username
+  },
+}
+
+export default {
+  state,
+  actions,
+  mutations,
+  getters
+}
