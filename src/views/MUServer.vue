7041c840a2aaad0634099fc87adb330ad2e42f71
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-11-28 18:51:24 -0500

    :rocket: Muestra los datos del 'mu_server' buscado por el parámetro 'id' de la ruta.

--- src/views/MUServer.vue
+++ src/views/MUServer.vue
@@ -2,5 +2,21 @@
   <div class="mu-server">
     Mu server
     {{ this.$route.params.id}}
+    {{ getCurrentMUServer }}
   </div>
 </template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters(['getCurrentMUServer']),
+  },
+  methods: {
+    ...mapActions(['fetchMUServer']),
+  },
+  created(){
+    this.fetchMUServer(this.$route.params.id)
+  }
+}
+</script>
