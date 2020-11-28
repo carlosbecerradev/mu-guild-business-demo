eae56f5625d4b0a3f1e1333a7cd4c56a3cde4a3c
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-11-28 18:49:19 -0500

    :rocket: Consume el método 'findMUServerbyId' de la API 'mu_servers'.

--- src/store/mu_servers.js
+++ src/store/mu_servers.js
@@ -2,6 +2,7 @@ import mu_servers_api from '@/api/mu_servers'
 
 const state = {
   all: [],
+  mu_server: null,
 }
 
 const actions = {
@@ -10,18 +11,33 @@ const actions = {
       commit('recieve_mu_servers', mu_servers)
     })
   },
+  async fetchMUServer({commit}, id){
+    try {
+      let response = await mu_servers_api.findMUServerbyId(id)
+      console.log('store', response)
+      commit('recieve_mu_server', response)
+    } catch (error) {
+      console.log(error)
+    }
+  },
 }
 
 const mutations = {
   recieve_mu_servers(state, mu_servers){
     state.all = mu_servers
-  }
+  },
+  recieve_mu_server(state, mu_server){
+    state.mu_server = mu_server
+  },
 }
 
 const getters = {
   allMUServers(state){
     return state.all
-  }
+  },
+  getCurrentMUServer(state){
+    return state.mu_server
+  },
 }
 
 export default {
