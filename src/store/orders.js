7a4d09ab99ac9aaee798c93dcd66743a8540454b
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-11-30 17:58:19 -0500

    :heavy_plus_sign: store: Creación de una 'store' para los pedidos.

---
+++ src/store/orders.js
@@ -0,0 +1,19 @@
+import orders_api from '@/api/orders'
+
+const state = {
+}
+const actions = {
+}
+
+const mutations = {
+}
+
+const getters = {
+}
+
+export default {
+  state,
+  actions,
+  mutations,
+  getters
+}
