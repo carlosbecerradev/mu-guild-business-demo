799beeeaa2fac1dc7e1b83a986d80ba321474515
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-11-30 16:48:22 -0500

    :rocket: components (MUServerContent): Cambio de componentes de los pedids, mis pedidos y historial de pedidos, con el uso del 'MUServerNavigation'.

--- src/components/mu-server/MUServerContent.vue
+++ src/components/mu-server/MUServerContent.vue
@@ -1,5 +1,36 @@
 <template>
   <div class="mu-server-content">
     <p>MU Server Content</p>
+    {{ getMUServerNavigationListItemSelected }}
+    <div v-if="getMUServerNavigationListItemSelected == getMUServerNavigationList[0]">
+      <MUServerOrders></MUServerOrders>
+    </div>
+    <div v-if="getMUServerNavigationListItemSelected == getMUServerNavigationList[1]">
+      <MUServerMyOrders></MUServerMyOrders>
+    </div>
+    <div v-if="getMUServerNavigationListItemSelected == getMUServerNavigationList[2]">
+      <MUServerOrderHistory></MUServerOrderHistory>
+    </div>
   </div>
 </template>
+
+<script>
+import { mapGetters } from "vuex";
+import MUServerOrders from "@/components/mu-server/MUServerOrders";
+import MUServerMyOrders from "@/components/mu-server/MUServerMyOrders";
+import MUServerOrderHistory from "@/components/mu-server/MUServerOrderHistory";
+
+export default {
+  computed: {
+    ...mapGetters([
+      "getMUServerNavigationList",
+      "getMUServerNavigationListItemSelected",
+    ]),
+  },
+  components: {
+    MUServerOrders,
+    MUServerMyOrders,
+    MUServerOrderHistory,
+  },
+};
+</script>
