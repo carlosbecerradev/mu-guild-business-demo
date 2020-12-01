fed55ea80d20ce087227956801959540f7db9e62
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-12-01 16:08:02 -0500

    :rocket: components (MUServerOrders): Template y se renderizan los pedidos de ese servidor mu.

--- src/components/mu-server/MUServerOrders.vue
+++ src/components/mu-server/MUServerOrders.vue
@@ -1,5 +1,89 @@
 <template>
   <div class="mu-server-orders">
-    Orders
+    <section class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 pt-2">
+      <h3
+        class="sr-only inline-block mb-2 text-sm leading-6 font-medium text-indigo-600 text-left py-1"
+      >
+        Results
+      </h3>
+
+      <!-- This example requires Tailwind CSS v2.0+ -->
+      <div v-if="getMUServerOrders != []" class="flex flex-col">
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div
+            class="py-4 align-middle inline-block min-w-full sm:px-6 lg:px-8"
+          >
+            <div class="overflow-hidden">
+              <table class="min-w-full divide-y divide-gray-200 border">
+                <thead>
+                  <tr>
+                    <th
+                      scope="col"
+                      class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Item
+                    </th>
+                    <th
+                      scope="col"
+                      class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      By
+                    </th>
+                    <th
+                      scope="col"
+                      class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Observation
+                    </th>
+                  </tr>
+                </thead>
+
+                <tbody class="bg-white divide-y divide-gray-200">
+                  <tr v-for="order in getMUServerOrders" :key="order.id">
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="flex items-center">
+                        <div class="">
+                          <div class="text-sm font-medium text-gray-900">
+                            {{order.item.name}} <span>+{{order.item_level}}</span>
+                          </div>
+                          <div class="text-sm text-gray-500">
+                            {{order.item_options.name}}
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="text-sm text-gray-900">{{order.user_account.nickname}}</div>
+                      <div class="text-sm text-gray-500">{{order.created}}</div>
+                    </td>
+                    <td class="px-6 py-4 text-sm text-gray-500">
+                      <p class="m-0 overflow-auto max-h-20 flex items-baseline">
+                        {{order.observation}}
+                      </p>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters(['getMUServerOrders'])
+  },
+  methods: {
+    ...mapActions(['fetchMUServerOrdersByMuServerId'])
+  },
+  created(){
+    this.fetchMUServerOrdersByMuServerId(this.$route.params.id)
+  }
+}
+</script>
