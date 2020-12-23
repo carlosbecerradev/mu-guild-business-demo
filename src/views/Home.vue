d46f236cd94ae2a55104f565c4f695352b2720f6
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-12-23 17:45:00 -0500

    :heavy_plus_sign: views (Home): Relleno.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,85 @@
 <template>
   <div class="home">
+    <section class="relative h-96">
+      <img
+        class="h-full w-full object-cover object-top"
+        src="../assets/mu-bg/mu-bg-1.jpg"
+      />
+      <div
+        class="absolute inset-0 w-full h-full bg-indigo-600 opacity-40"
+      ></div>
+      <div class="absolute inset-0 flex flex-col justify-center items-center">
+        <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 my-6 text-center">
+          <p
+            class="text-4xl sm:text-7xl font-extrabold text-white font-serif tracking-wider"
+          >
+            Guild Name
+          </p>
+          <p
+            class="text-4xl sm:text-7xl font-extrabold text-white font-serif tracking-wider"
+          >
+            Guild Name
+          </p>
+          <p
+            class="text-4xl sm:text-7xl font-extrabold text-white font-serif tracking-wider"
+          >
+            Guild Name
+          </p>
+        </div>
+      </div>
+    </section>
+    <nav class="bg-white border-b">
+      <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 py-2">
+        <span>News</span>
+      </div>
+    </nav>
+    <section class="my-4">
+      <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div class="grid grid-cols-8 gap-6">
+          <div class="col-span-8 sm:col-span-5 md:col-span-6">
+            <div class="">
+              <div class="grid grid-cols-12 gap-4">
+                <div class="col-span-12 md:col-span-6">
+                  <div class="p-4 bg-white shadow-sm">
+                    <div class="title text-lg font-bold border-b mb-4">
+                      New 1
+                    </div>
+                    <p class="text-sm">Lorem ipsum dolor sit amet.</p>
+                  </div>
+                </div>
+                <div class="col-span-12 md:col-span-6">
+                  <div class="p-4 bg-white shadow-sm">
+                    <div class="title text-lg font-bold border-b mb-4">
+                      New 2
+                    </div>
+                    <p class="text-sm">Lorem ipsum dolor sit amet.</p>
+                  </div>
+                </div>
+                <div class="col-span-12 md:col-span-6">
+                  <div class="p-4 bg-white shadow-sm">
+                    <div class="title text-lg font-bold border-b mb-4">
+                      New 3
+                    </div>
+                    <p class="text-sm">Lorem ipsum dolor sit amet.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <aside class="Ads my-5">
+              <a href="https://gamesdonequick.com" target="_blank">
+                <img src="https://i.imgur.com/A5gttc8.png" alt="" />
+              </a>
+            </aside>
+          </div>
+          <aside class="hidden sm:block sm:col-span-3 md:col-span-2">
+            <div class="p-4 border border-gray-200">
+              <div class="some-ads">
+                <div class="h-96 w-full text-center">Some Ads</div>
+              </div>
+            </div>
+          </aside>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
