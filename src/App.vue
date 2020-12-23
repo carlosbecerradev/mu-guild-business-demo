013159850b0c1504b2cd8a894e7954c1cccd30e8
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-12-23 15:42:44 -0500

    :rocket: App: Se agrego el 'footer'. Cambios en el css y etiquetas html.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
-    <div class="bg-gray-50 min-h-screen relative">
-      <MainNavbar></MainNavbar>
-      <router-view/>
+    <div class="bg-gray-50 min-h-screen flex flex-col">
+      <main-navbar />
+      <router-view />
+      <main-footer class="mt-auto" />
     </div>
   </div>
 </template>
 
 <script>
-import MainNavbar from '@/components/main/MainNavbar'
+import MainNavbar from "@/components/main/MainNavbar";
+import MainFooter from "@/components/main/MainFooter";
 
 export default {
   components: {
     MainNavbar,
+    MainFooter,
   },
-}
+};
 </script>
