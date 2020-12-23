06ce38e1463e3c09e07c644292968f88338e16d1
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-12-23 15:40:14 -0500

    :heavy_plus_sign: Creación de un 'footer'.

---
+++ src/components/main/MainFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer>
+    <div class="bg-gray-800">
+      <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 py-6">
+        <div class="text-center text-white">
+          <span>footer</span>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
