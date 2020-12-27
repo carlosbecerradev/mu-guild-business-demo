8326e5d077bece2585e80a7e7c4311bd69398a6e
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-12-27 16:20:37 -0500

    :heavy_plus_sign: components (MUServer): Mostrar botón de nuevo pedido solo si ha iniciado sesión.

--- src/views/MUServer.vue
+++ src/views/MUServer.vue
@@ -93,7 +93,7 @@
               </div>
             </div>
           </div>
-          <div class="mt-5 flex md:mt-0 md:ml-4">
+          <div v-if="getUsername != null" class="mt-5 flex md:mt-0 md:ml-4">
             <button
               @click="toggleMUServerModal({ name: 'order', action: 'new' })"
               class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
@@ -131,7 +131,7 @@ import MUServerModal from "@/components/mu-server/MUServerModal";
 
 export default {
   computed: {
-    ...mapGetters(["getCurrentMUServer", "getMUServerModal"]),
+    ...mapGetters(["getCurrentMUServer", "getMUServerModal", "getUsername"]),
   },
   methods: {
     ...mapActions(["fetchMUServer", "toggleMUServerModal"]),
